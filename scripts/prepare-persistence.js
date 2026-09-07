087ae4a5077dafaa0bf8ecea2f64b14153e208bc
TatanBelt <109092099+TatanBelt@users.noreply.github.com>
2026-09-07 11:15:48 -0500

    Run stability corrections during startup

--- scripts/prepare-persistence.js
+++ scripts/prepare-persistence.js
@@ -38,3 +38,4 @@ patchFile(appPath, [
 
 console.log('Persistencia preparada: SQLite=/data, adjuntos=/uploads, cotizaciones=/quotations dentro de STORAGE_DIR.');
 require('./bootstrap-extra-admin');
+require('./patch-stability');
